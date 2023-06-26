db79b4dd438f8383bd3b997d250351a88430e136
Subham Subhasis Patra <43811917+SubhamSubhasisPatra@users.noreply.github.com>
2023-06-26 23:48:20 +0530

    added how to remove element from the array in O(n)

--- src/Array/remove_element.py
+++ src/Array/remove_element.py
@@ -1,19 +1,11 @@
-def move_to_end(arr, index, ele):
-    pass
-
 
 def remove_element(nums, element):
-    counter = 0
-
-    for index in range(len(nums) - 1):
-        cur_number = nums[index]
-        if cur_number == element:
-            counter += 1
-            nums.pop(index)
-            nums.append(cur_number)
-
-            # move the element to the end of the arr but the condition is
-            # how to also take care of the number which are not same as the given number
+    c = 0
+    for i in range(nums):
+        if i != element:
+            nums[c] = i  
+            c += 1 
+            
     print(nums, counter)
 
 
