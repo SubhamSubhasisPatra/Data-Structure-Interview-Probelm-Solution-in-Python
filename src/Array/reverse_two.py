9e0e520bcbcca11af22eee782d819f6e750976a7
Subham Subhasis Patra <subhasispatra94@gmail.com>
2023-08-19 10:59:23 +0530

    Vowel reverse with extra space

--- src/Array/reverse_two.py
+++ src/Array/reverse_two.py
@@ -1,5 +1,7 @@
+vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+
+
 def reverse_vowels(s):
-    vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
     s_list = list(s)
     left, right = 0, len(s) - 1
 
@@ -17,9 +19,29 @@ def reverse_vowels(s):
     return ''.join(s_list)
 
 
+def reverse_vowel_non_optimal(s):
+    v_list = []
+
+    result = ''
+
+    for v in s:
+        if v in vowels:
+            v_list.append(v)
+
+    for ele in s:
+        if ele not in vowels:
+            result += ele
+        else:
+            result += v_list.pop()
+
+    return result
+
+
 if __name__ == '__main__':
     s1 = "hello"
     print(reverse_vowels(s1))  # Output: "holle"
+    print(reverse_vowel_non_optimal(s1))  # Output: "holle"
 
     s2 = "leetcode"
     print(reverse_vowels(s2))  # Output: "leotcede"
+    print(reverse_vowel_non_optimal(s2))  # Output: "leotcede"
