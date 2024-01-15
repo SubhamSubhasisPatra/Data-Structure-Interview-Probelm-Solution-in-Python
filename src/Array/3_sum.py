99689f8ba64df68eadc481a33de6cf5068d8c6f3
Subham Subhasis Patra <subhasispatra94@gmail.com>
2024-01-15 11:25:28 +0530

    daily challenge for leetcode , added optimal solution for 3Sum

--- src/Array/3_sum.py
+++ src/Array/3_sum.py
@@ -22,5 +22,28 @@ def three_sum(arr):
     print(result)
 
 
+def optimal_3_sum(nums):
+    nums = sorted(nums)
+    res = set()
+
+    for i in range(len(nums)):
+        l = i + 1
+        r = len(nums) - 1
+
+        target = 0 - nums[i]
+
+        while l < r :
+            if nums[l] + nums[r] == target:
+                res.add((nums[i], nums[l], nums[r]))
+                l += 1
+                r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                r -= 1
+    return list(res)
+
+
 if __name__ == '__main__':
-    print(three_sum([-1, 0, 1, 2, -1, -4]))
+    # print(three_sum([-1, 0, 1, 2, -1, -4]))
+    print(optimal_3_sum([-1, 0, 1, 2, -1, -4]))
